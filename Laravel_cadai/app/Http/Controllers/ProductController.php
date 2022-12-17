45f0c49e2feab74b4ce8bf9c4dc06028eecf8c66
udamasa0616 <101563907+udamasa0616@users.noreply.github.com>
2022-12-17 16:06:09 +0900

    12月17日16:06更新

--- Laravel_cadai/app/Http/Controllers/ProductController.php
+++ Laravel_cadai/app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Products;
+use App\Models\Product;
 
 
 class ProductController extends Controller
--- Laravel_cadai/app/Models/Product.php
+++ Laravel_cadai/app/Models/Product.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class Products extends Model
+class Product extends Model
 {
     // テーブルからデータを取得
     public function getAll()
