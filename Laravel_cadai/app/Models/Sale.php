fba8df060ba48d26ea519746a95a3d07c7128789
udamasa0616 <101563907+udamasa0616@users.noreply.github.com>
2022-12-17 16:52:42 +0900

    17:00

--- Laravel_cadai/app/Models/Sale.php
+++ Laravel_cadai/app/Models/Sale.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
@@ -10,7 +10,7 @@ class Sale extends Model
     // テーブルからデータを取得
     public function getAll()
     {
-        $sales = DB::table($this->table)
+        $sales = DB::table($this->sales)
             ->select(
                 'user_name',
                 'email',
